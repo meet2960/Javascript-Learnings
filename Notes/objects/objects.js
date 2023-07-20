5d1b9609ecf2cdc25d6aeb0e27d1d8a755df4e8f
meet2960 <meetghelani43@gmail.com>
2023-07-20 15:54:24 +0530

    Added new methods examples

--- Notes/objects/objects.js
+++ Notes/objects/objects.js
@@ -18,13 +18,13 @@ carObj["model"] = "7 Series";
 console.log("New obj", carObj);
 
 // * Delete any properties of object
-let newObj = {
+let delObj = {
   prop1: "value1",
   prop2: "value2",
 };
-console.log("NewObj", newObj);
-delete newObj.prop2; // it will delete whole property with its value // delete always return true
-console.log("NewObj", newObj);
+console.log("delObj", delObj);
+delete delObj.prop2; // it will delete whole property with its value , and delete always return true
+console.log("delObj", delObj);
 
 // * Object with methods,
 let personObj = {
@@ -37,7 +37,7 @@ let personObj = {
 console.log(`Current age is ${personObj.ageCalculator()}`);
 
 // * This keyword
-let personThisObj = {
+let simpleFunObj = {
   name: "JavaScript",
   getName: function () {
     // console.log(`Hello ${name} Welcome to Party !!!`);
@@ -45,27 +45,58 @@ let personThisObj = {
     return this.name;
   },
 };
-console.log(`Access Name ${personThisObj.getName()}`);
+console.log(`Access Name ${simpleFunObj.getName()}`);
 
-// * Using Arrow function with this, will always refer to window obj, not the person object
-let myPerson = {
+// * Using Arrow function with this, will always refer to window obj or the top global scope, not the person object
+let arrowFunObj = {
   name: "John",
   getName: () => {
     console.log(`Person Name is ${this.name}`);
     return this.name;
   },
 };
-myPerson.getName();
+arrowFunObj.getName();
 
 // * Iteration on Object
-let newCarObj = {
+let iterationObj = {
   company: "BMW",
   model: "5 Series",
   color: "Black",
 };
-console.log("Car object is : ", newCarObj);
+console.log("Car object is : ", iterationObj);
 // ? To Iterate on this object, we can use for in loop
-for (let key in newCarObj) {
+for (let key in iterationObj) {
   console.log("Key : ", key); // We get all the key
-  console.log("Value : ", newCarObj[key]); // we get all the values
+  console.log("Value : ", iterationObj[key]); // we get all the values
 }
+
+// * If We want to add an Symbol to our Obj, we can do by this syntax :-
+const mySymbol = Symbol("Key1");
+const newSymObj = {
+  name: "Symbol Object Addition ",
+  [mySymbol]: "mySymbolKey1",
+};
+console.log("newSymObj", newSymObj);
+
+// * Merging Objects or Combine Objects
+const obj1 = { 1: "a", 2: "b" };
+const obj2 = { 3: "c", 4: "d" };
+const obj3 = Object.assign({}, obj1, obj2); // This gurantees that the final result will be only object, so we have to use {} in assign function  , not used this method mostly
+console.log("Combined using Assign", obj3);
+const combinedObj = { ...obj1, ...obj2 }; // used widley in production
+console.log("Combined using Spread", combinedObj);
+
+// * Create object using constructor (also called singleton object)
+const twitterUserObj = new Object();
+twitterUserObj.id = 111;
+twitterUserObj.name = "Constructor Object";
+twitterUserObj.isLoggedIn = false;
+console.log("twitterUserObj", twitterUserObj);
+
+console.log("Keys", Object.keys(twitterUserObj)); // Returns all keys in form of array
+
+console.log("Values", Object.values(twitterUserObj)); // Returns all values in form of array
+
+console.log("Entries", Object.entries(twitterUserObj)); // Returns whole object as array and key and value as an array to
+
+console.log("Check Property", twitterUserObj.hasOwnProperty("isUser")); // Returns true if property exist inside object and return false if property doesn't exist inside object
