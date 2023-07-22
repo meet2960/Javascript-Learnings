f035311c6f9d395bb8338586e9206aa95e62eb53
meet2960 <meetghelani43@gmail.com>
2023-07-22 17:51:59 +0530

    added some new lines

--- Notes/modules/exports/objexport.js
+++ Notes/modules/exports/objexport.js
@@ -1,4 +1,4 @@
-const a = {
+const newObj = {
   average: (a, b) => {
     console.log((a + b) / 2);
   },
@@ -6,4 +6,16 @@ const a = {
     console.log((a / b) * 100);
   },
 };
-module.exports = a;
+
+module.exports = newObj;
+
+const anotherObj = {
+  name: "BMW",
+  model: "5 Series",
+};
+// exports.data = anotherObj; //
+// exports.data = { newObj, anotherObj }; // Export multiple values together
+// To import above kind of export, desctructure the data and then we get all the properties from it
+exports.thirdObj = {
+  name: "Direct Exports",
+};
