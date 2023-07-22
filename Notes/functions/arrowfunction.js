ae661a53534c7c4369d2e56ad48162bdebb20152
meet2960 <meetghelani43@gmail.com>
2023-07-22 11:51:45 +0530

    Added new example

--- Notes/functions/arrowfunction.js
+++ Notes/functions/arrowfunction.js
@@ -1,9 +1,19 @@
 // * Arrow Functions
 
 let exampleArrowFunction = (name) => {
-  console.log(`Welcome! ${name} You are Invited`);
+  // console.log(`Welcome! ${name} You are Invited`);
   return name;
 };
 const returnedName = exampleArrowFunction("harry");
-console.log("Name", returnedName);
-console.log(exampleArrowFunction("meet"));
+// console.log("Name", returnedName);
+// console.log(exampleArrowFunction("meet"));
+
+const newArrowFunction = () => {
+  let username = "javascript";
+  console.log(this); // {}
+};
+newArrowFunction();
+
+// * Implicit Return, no need to write curly braces and return keyword
+const addTwo = (num1, num2) => num1 + num2;
+console.log(addTwo(3, 4));
