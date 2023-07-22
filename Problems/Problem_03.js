d507c9b018109bc62291f31b014a45f048de2e4e
meet2960 <meetghelani43@gmail.com>
2023-07-22 11:58:07 +0530

    Chaining of Function

--- Problems/Problem_03.js
+++ Problems/Problem_03.js
@@ -1,4 +1,6 @@
 // ? Example of function chaining and do some operation based on the function provided
+// * Function Chaining means calling one function with another and it forms a chain like structure.
+
 const calc = {
   total: 0,
   add(a) {
@@ -22,4 +24,4 @@ const calc = {
 const result = calc.add(100).multiply(5).subtract(250).add(50).divide(2);
 console.log(result.total);
 
-// ? Here returning this in every object, because everytime the next function is called, we want the updated value of total, so to get that, we are returning this in every function.
+// ? Here we are returning this in every object, because everytime the next function is called, we want the updated value of total, so to get that, we are returning this in every function.
